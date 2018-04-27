6752068df99ef0ba8ad0cdbf4e201479121baa54
pix boly <pixboly@gmail.com>
2018-04-27 13:32:42 +0800

    modify base shell

--- base_install.sh
+++ base_install.sh
@@ -1,8 +1,9 @@
-sudo apt-get install vim
-sudo apt-get install net-tools
-sudo apt-get install git
-sudo apt-get install curl
-sudo apt-get install autoconf automake libtool
-sudo apt-get install make
-sudo apt-get install build-essential   
-sudo apt-get install g++
+sudo apt-get install vim -y
+sudo apt-get install net-tools -y
+sudo apt-get install net-tools -y
+sudo apt-get install git -y
+sudo apt-get install curl -y
+sudo apt-get install autoconf automake libtool -y
+sudo apt-get install make -y
+sudo apt-get install build-essential -y   
+sudo apt-get install g++ -y
